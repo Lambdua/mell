74010f096443c5ffbe7f9581a25e03d65e892814
liangt <lt547670718@gmail.com>
2020-03-03 10:31:52 +0800

    123

---
+++ src/main/java/lt/school/mell/MellApplication.java
@@ -0,0 +1,13 @@
+package lt.school.mell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/school/mell/MellApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.school.mell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
